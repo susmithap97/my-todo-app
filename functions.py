cabaed236ccd7ac3f6a3b643ce7e9bec40821162
Susmitha Palavajjhala <susmithapalavajjhala97@gmail.com>
2023-03-01 19:45:22 +0000

    Initial commit 1

--- functions.py
+++ functions.py
@@ -1,6 +1,5 @@
 
-#FILEPATH = "todos.txt"
-FILEPATH = "/Users/susmithapalavajjhala/Documents/Python_Projects_Learn/venv/todos.txt"
+FILEPATH = "todos.txt"
 def get_todos(filepath=FILEPATH):
     with open(filepath, 'r') as file_local:
         todos_local = file_local.readlines()
